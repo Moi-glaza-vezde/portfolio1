b31f9dd5183bd61bc0bcafb6c3ffad8d6caceffe
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2024-12-04 07:05:52 +0300

    Первый коммит

---
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Icon } from '../../../../components/icon/Icon';
+import styled from 'styled-components';
+
+type SkillPropsType = {
+   iconId: string;
+   title: string;
+   description: string;
+};
+export const Skill = (props: SkillPropsType) => {
+   return (
+      <StyledSkill>
+         <Icon iconId={props.iconId} />
+
+         <SkillTitle>{props.title}</SkillTitle>
+         <SkillText>{props.description}</SkillText>
+      </StyledSkill>
+   );
+};
+
+const StyledSkill = styled.div`
+   width: 30%;
+   background-color: #fa0a0a31;
+   margin: 10px;
+`;
+const SkillTitle = styled.div``;
+const SkillText = styled.div``;
