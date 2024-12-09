42523ca97b7a60efd38c3ef47d9b0f18be72c3c2
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2024-12-09 17:01:25 +0500

    header part2 done

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { Logo } from '../../components/logo/Logo';
 import { Menu } from '../../components/menu/Menu';
 import { Container } from '../../components/Container';
+import { FlexWrapper } from '../../components/FlexWrapper';
+import { HeaderMenu } from './headerMenu/HeaderMenu';
 
 const items = ['Home', 'Skills', 'Works', 'Testimony', 'Contact'];
 
@@ -10,14 +12,22 @@ export const Header = () => {
    return (
       <StyledHeader>
          <Container>
-            <Logo />
-            <Menu menuItems={items} />
+            <FlexWrapper justify={'space-between'} align={'center'}>
+               <Logo />
+               <HeaderMenu menuItems={items} />
+            </FlexWrapper>
          </Container>
       </StyledHeader>
    );
 };
 const StyledHeader = styled.header`
-   background-color: #21d139;
+   background-color: #252527;
    display: flex;
    justify-content: space-between;
+   padding: 20px 0;
+   position: fixed;
+   top: 0;
+   left: 0;
+   right: 0;
+   z-index: 10;
 `;
